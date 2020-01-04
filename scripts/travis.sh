83a1891d095f5302657e62c5ba53112bbb2303db
Yury Gribov <tetra2005@gmail.com>
2020-01-04 04:06:31 +0300

    Update CI.

---
+++ scripts/travis.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# The MIT License (MIT)
+# 
+# Copyright (c) 2018-2020 Yury Gribov
+# 
+# Use of this source code is governed by The MIT License (MIT)
+# that can be found in the LICENSE.txt file.
+
+set -eu
+set -x
+
+make all
+./setup.py build
+./setup.py bdist_wheel
