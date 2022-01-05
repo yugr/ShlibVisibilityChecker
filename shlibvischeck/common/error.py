476696c8fee22d35b01c5a0c932afa2e3a10de21
Yuri Gribov <tetra2005@gmail.com>
2022-01-05 11:46:24 +0300

    Added function docstrings.

--- shlibvischeck/common/error.py
+++ shlibvischeck/common/error.py
@@ -16,9 +16,11 @@ _except = False
 _me = os.path.basename(sys.argv[0])
 
 def warn(msg):
+  "Emit nicely-formatted warning"
   sys.stderr.write("%s: warning: %s\n" % (_me, msg))
 
 def error(msg):
+  "Emit nicely-formatted error and abort"
   sys.stderr.write("%s: error: %s\n" % (_me, msg))
   global _except
   if _except:
@@ -26,9 +28,11 @@ def error(msg):
   sys.exit(1)
 
 def set_basename(name):
+  "Set basename for error/warning reports"
   global _me
   _me = name
 
 def set_throw_on_error(v=True):
+  "Set behavior on error"
   global _except
   _except = v
